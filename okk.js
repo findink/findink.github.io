10b7d63b21ee8add189203a48ee431daf80ab1e8
lgq <47750446+findink@users.noreply.github.com>
2019-02-20 15:34:41 +0800

    Create okk.js

---
+++ okk.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function okk(){
+alert('okk!!')
+}
+
+function comp(){
+	var k1 = document.getElementById('k1');
+	var k2 = document.getElementById('k2');
+	var as = document.getElementById('as');
+	as.innerHTML = parseFloat(k1.value)  + parseFloat(k2.value) ;
+}
